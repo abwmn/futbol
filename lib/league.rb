435e5b64fc4c8bd16b8f9b9ae880ca40d63824a0
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-06 06:19:38 -0500

    cleanup

--- lib/league.rb
+++ lib/league.rb
@@ -24,30 +24,6 @@ class League < StatBook
     }
   end
 
-  def favorite_opponent(team)
-
-  end
-
-  def rival(team)
-
-  end
-
-  def biggest_team_blowout(team)
-
-  end
-
-  def worst_loss(team)
-
-  end
-
-  def head_to_head(team)
-
-  end
-
-  def seasonal_summary(team)
-
-  end
-
   ## Helper Method ##
   
   def index(team)
@@ -55,18 +31,6 @@ class League < StatBook
   end
 end
 
-
 # Method	Description	Return Value
-
-# favorite_opponent	
-    # Name of the opponent that has the lowest win percentage against the given team.	String
-# rival	
-    # Name of the opponent that has the highest win percentage against the given team.	String
-# biggest_team_blowout	
-    # Biggest difference between team goals and opponent goals for a win for the given team.	Integer
-# worst_loss	
-    # Biggest difference between team goals and opponent goals for a loss for the given team.	Integer
-# head_to_head	
-    # Record (as a hash - win/loss) against all opponents with the opponents’ names as keys and the win percentage against that opponent as a value.	Hash
 # seasonal_summary	
     # For each season that the team has played, a hash that has two keys (:regular_season and :postseason), that each point to a hash with the following keys: :win_percentage, :total_goals_scored, :total_goals_against, :average_goals_scored, :average_goals_against.	Hash
